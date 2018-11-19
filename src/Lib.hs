e4405f3a0baf81172d71af7f1b199af1e9c9fa02
Michał Wypych <mwypych@agh.edu.pl>
2018-11-19 20:07:13 +0100

    Adds save and exists implementation; Connection with Cassandra works perfectly

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,35 @@
 module Lib
-    ( someFunc
+    ( someFunc,
+      connectToCassandraRepositoryAndFindStates
     ) where
+
+import qualified Repository
+import qualified Data.UUID as Uuid
+import Data.Text (Text)
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+connectToCassandraRepositoryAndFindStates :: IO ()
+connectToCassandraRepositoryAndFindStates = do
+    repository <- Repository.new
+    first <- Repository.findById (uuid "35f32256-9217-4475-bd99-fc8c3ef225e5") repository
+    second <- Repository.findById (uuid "a343ef97-da23-4f58-bf5c-cd9d33224e12") repository    
+    print first
+    print second
+    existsSecond <- Repository.exists (uuid "a343ef97-da23-4f58-bf5c-cd9d33224e12") repository  
+    print existsSecond
+    let updatedSecond = withValue "nowa wartosc ktora dopiero teraz powinna byc widoczna" (fromJust second)
+    Repository.save updatedSecond repository
+    Repository.destroy repository
+
+
+fromJust :: Maybe a -> a
+fromJust (Just v) = v
+fromJust Nothing = error "it is not possible to extract value from Nothing"
+
+uuid :: String -> Uuid.UUID
+uuid = fromJust . Uuid.fromString
+
+withValue :: Text -> Repository.State -> Repository.State
+withValue newValue (Repository.State uuid _) = Repository.State uuid newValue
--- src/Repository.hs
+++ src/Repository.hs
@@ -6,43 +6,96 @@ import Data.Text (Text)
 import Data.Functor.Identity
 import Database.CQL.IO as Client
 import Database.CQL.Protocol as Protocol
+
 import Data.UUID (UUID)
 import Data.Either.Combinators
 import Control.Monad.IO.Class
+import qualified System.Random as Rand
+import qualified Data.UUID as Uuid
 import qualified System.Logger as Logger
 
-data State = State UUID Text 
+import Debug.Trace
+
+data State = State UUID Text deriving (Eq, Show)
 
 
 toState :: Either String Row -> Either String State
 toState monadicRow = do
-     row <- monadicRow
+     row <- trace (show monadicRow) monadicRow
+    -- trace (show row) (return ())
      uuid <- fromRow 0 row
+--     trace (show uuid) (return ())
      value <- fromRow 1 row
-     return $ State uuid value
+     return $ State (fromJust uuid) (fromJust value)
 
 
 class StateRepository repository where
-  save :: State -> repository -> State
-  exists :: State -> repository -> Bool
-  findById :: MonadIO m =>  UUID -> repository -> m (Maybe State)
+  save :: MonadIO m => State -> repository -> m ()
+  exists :: MonadIO m => UUID -> repository -> m Bool
+  findById :: MonadIO m => UUID -> repository -> m (Maybe State)
   
 
-data CassandraStateRepository = CassandraStateRepository {connection::ClientState, findByIdQuery :: QueryString R (Identity UUID) (Row), toFindByIdQueryParams :: UUID -> QueryParams (Identity UUID)}
+data CassandraStateRepository = CassandraStateRepository {
+        connection::ClientState, 
+        findByIdQuery :: QueryString R (Identity UUID) (Row), 
+        toFindByIdQueryParams :: UUID -> QueryParams (Identity UUID),
+        existsQuery :: QueryString R (Identity UUID) (Identity UUID), 
+        existsQueryParams :: UUID -> QueryParams (Identity UUID),
+        updateQuery :: QueryString W (Text, UUID) (), 
+        updateQueryParams :: UUID -> Text -> QueryParams (Text, UUID),
+        saveQuery :: QueryString W (UUID, Text) (), 
+        saveQueryParams :: UUID -> Text -> QueryParams (UUID, Text)}
 
 new :: MonadIO m => m CassandraStateRepository
 new = do 
     logger <- Logger.new Logger.defSettings 
     connection <- Client.init logger Client.defSettings 
-    return $ CassandraStateRepository connection findByIdQuery findByIdQueryParams
+    return $ CassandraStateRepository connection findByIdQuery findByIdQueryParams existsQuery existsQueryParams updateQuery updateQueryParams saveQuery saveQueryParams
    where
-    findByIdQuery = QueryString . LT.pack $ "select * from alvis.stored_system_state WHERE id = ?"
+
+    findByIdQuery = QueryString . LT.pack $ "SELECT * from alvis_test.stored_system_state WHERE id = ?"
     findByIdQueryParams uuid = defQueryParams One (Identity uuid)
 
-instance StateRepository (CassandraStateRepository) where
-  findById id (CassandraStateRepository connection findByIdQuery findByIdQueryParams) = do
-    let params = findByIdQueryParams id
-    maybeRow <- runClient connection (query1 findByIdQuery params)
-    return . rightToMaybe . toState . maybeToRight "the state was not found" $ maybeRow
-  
+    existsQuery = QueryString . LT.pack $ "SELECT id from alvis_test.stored_system_state WHERE id = ?"
+    existsQueryParams uuid = defQueryParams One (Identity uuid)
 
+    updateQuery = QueryString . LT.pack $ "UPDATE  alvis_test.stored_system_state SET value = ? WHERE id = ?"
+    updateQueryParams uuid value = defQueryParams One (value, uuid)
+
+    saveQuery = QueryString . LT.pack $ "INSERT INTO alvis_test.stored_system_state (id, value) VALUES (?, ?)"
+    saveQueryParams uuid value = defQueryParams One (uuid, value)
+
+destroy :: MonadIO m => CassandraStateRepository -> m ()
+destroy (CassandraStateRepository connection _ _ _ _ _ _ _ _) = shutdown connection
+
+instance StateRepository (CassandraStateRepository) where
+  findById id (CassandraStateRepository connection findByIdQuery findByIdQueryParams _ _ _ _ _ _) = do
+    let params = findByIdQueryParams id
+    maybeRow <- runClient connection (query1 findByIdQuery params)   
+    return . rightToMaybe . toState . maybeToRight "the state was not found" $ maybeRow
+
+  exists id (CassandraStateRepository connection _ _ existsQuery existsQueryParams _ _ _ _) = do
+    let params = existsQueryParams id
+    maybeRow <- runClient connection (query1 existsQuery params)   
+    return . isJust $ maybeRow
+
+  save (State uuid value) (CassandraStateRepository connection _ _ _ _ updateQuery updateQueryParams saveQuery saveQueryParams) = do
+    case Uuid.null uuid of
+      True -> do
+        newUuid <- generateUuid
+        let params = saveQueryParams newUuid value
+        runClient connection (write saveQuery params)   
+      False -> do
+        let params = updateQueryParams uuid value
+        runClient connection (write updateQuery params)   
+
+generateUuid :: MonadIO m => m UUID
+generateUuid = liftIO $ Rand.getStdRandom Rand.random
+
+fromJust :: Maybe a -> a
+fromJust (Just v) = v
+fromJust Nothing = error "it is not possible to extract value from Nothing"
+  
+isJust :: Maybe a -> Bool
+isJust (Just _) = True
+isJust Nothing = False
